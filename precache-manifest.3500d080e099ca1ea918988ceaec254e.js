223fee2f97b363db4f0b0562362644392548b53e
YasirSherzad <ys.sherzad@gmail.com>
2020-07-09 21:17:06 +0430

    Updates

---
+++ precache-manifest.3500d080e099ca1ea918988ceaec254e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1ac6d0e91d9d7ef40d0b91c7de7e7e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6db222c6688b1e74b213",
+    "url": "/monsters-rolodex/static/css/main.98c3b228.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db222c6688b1e74b213",
+    "url": "/monsters-rolodex/static/js/main.b9b28adb.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
